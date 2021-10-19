80b7d90612aea9459dd6e83b2545e0304d6b9c6e
jyxjjj <773933146@qq.com>
2021-10-19 11:35:19 +0800

    cd

--- zshrc.d/99.chpwd.sh
+++ zshrc.d/99.chpwd.sh
@@ -1,9 +1,14 @@
-cd() {
+export SETEDPHPFORMP=0
+
+function cd() {
     builtin cd $1
     ls -alh
     case $PWD in
         (*/KJ/mp*)
-            setphp 7.3
+            if (( SETEDPHPFORMP == 0 )) {
+                setphp 7.3
+                export SETEDPHPFORMP=1
+            }
             ;;
     esac
 }
