405e3e202037cc441a17e987fddd6352545a219f
jyxjjj <773933146@qq.com>
2022-02-24 13:24:23 +0800

    Update 31.functions.sh

--- zshrc.d/31.functions.sh
+++ zshrc.d/31.functions.sh
@@ -32,15 +32,15 @@ function tbw() {
 }
 
 function listip() {
-    echo "Internet:                                   $(curl -s https://api.ipify.org)"
-    echo "Internet:                                   $(curl -s https://api6.ipify.org)"
-    echo "en0(Ethernet)         (14:98:77:47:c5:07):  $(ipconfig getifaddr en0)"
-    echo "en1(Wi-Fi)            (14:98:77:5f:86:4f):  $(ipconfig getifaddr en1)"
-    echo "en2(Thunderbolt 1)    (36:cf:8b:2d:89:84):  $(ipconfig getifaddr en2)"
-    echo "en3(Thunderbolt 2)    (36:cf:8b:2d:89:80):  $(ipconfig getifaddr en3)"
-    echo "en4(Ethernet Adaptor) (1e:00:33:3c:c8:54):  $(ipconfig getifaddr en4)"
-    echo "en5(Ethernet Adaptor) (1e:00:33:3c:c8:55):  $(ipconfig getifaddr en5)"
-    echo "en6(Bluetooth PAN)    (14:98:77:4a:59:9a):  $(ipconfig getifaddr en6)"
+    echo "Internet4:                                  $(curl -s https://api.ipify.org)"
+    echo "Internet6:                                  $(curl -s https://api6.ipify.org)"
+    echo "en0(Wi-Fi)            (f4:d4:88:6e:06:b1):  $(ipconfig getifaddr en0)"
+    echo "en1(Thunderbolt 1)    (36:85:02:21:fc:00):  $(ipconfig getifaddr en1)"
+    echo "en2(Thunderbolt 2)    (36:85:02:21:fc:04):  $(ipconfig getifaddr en2)"
+    echo "en3(Thunderbolt 3)    (36:85:02:21:fc:08):  $(ipconfig getifaddr en3)"
+    echo "en4(Ethernet Adaptor) (ae:9c:e1:cb:6e:79):  $(ipconfig getifaddr en4)"
+    echo "en5(Ethernet Adaptor) (ae:9c:e1:cb:6e:7a):  $(ipconfig getifaddr en5)"
+    echo "en6(Ethernet Adapter) (ae:9c:e1:cb:6e:7b):  $(ipconfig getifaddr en6)"
 }
 
 function rmDSStore () {
@@ -48,6 +48,8 @@ function rmDSStore () {
     sudo find . -name '._.DS_Store' -delete 2>/dev/null
 }
 
-function webp2png(){
-    ffmpeg -i $1 $1.png
+function filehash () {
+    sha1 $1
+    sha256 $1
+    sha512 $1
 }
