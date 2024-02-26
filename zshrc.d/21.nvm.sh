6f7bd659c613f6de676fa97dbebdf3e722e9491d
jyxjjj <773933146@qq.com>
2024-02-26 11:53:45 +0800

    f

--- zshrc.d/21.nvm.sh
+++ zshrc.d/21.nvm.sh
@@ -20,6 +20,7 @@ function nvmupdate() {
     echo "Update available: $old -> $new"
     echo "Do you want to proceed? (Y/n)\c"
     read -q -k 1 response
+    echo -n "\n"
     if [[ $response == "N" || $response == "n" ]]; then
         echo "Aborting."
         return
