2cae0020855bcc5edbfa54a65b6751ffaa2ea64d
jyxjjj <773933146@qq.com>
2025-05-21 17:32:04 +0800

    2025-05-21 17:32:04

--- zshrc.d/02.ssh.sh
+++ zshrc.d/02.ssh.sh
@@ -1,5 +1,11 @@
 alias ssh='ssh -2 -4'
 
-export SSHHosts=( $( grep ^Host ~/.ssh/hosts.ssh | awk '{print $2}' | tr '\n' ' ') )
+rm -f "$HOME/.ssh/known_hosts"
+
+export SSHHosts=($(grep ^Host ~/.ssh/hosts.ssh | awk '{print $2}' | tr '\n' ' '))
 
 complete -W "$SSHHosts" ssh
+
+function cleanSSH() {
+    rm -f "$HOME/.ssh/control-"*
+}
