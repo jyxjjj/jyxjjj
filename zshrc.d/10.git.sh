c4ee340d4b0a8c3a7e8fd5ab1409cfc7cc908068
jyxjjj <773933146@qq.com>
2025-10-16 14:40:30 +0800

    2025-10-16 14:40:30

--- zshrc.d/10.git.sh
+++ zshrc.d/10.git.sh
@@ -12,13 +12,10 @@ function githubdesktop() {
         echo "Usage: githubdesktop <path>"
         return
     fi
-    BAT="/Applications/GitHub Desktop.app/Contents/Resources/app/static/github.sh"
     BIN="/Applications/GitHub Desktop.app/Contents/MacOS/GitHub Desktop"
     CLI="/Applications/GitHub Desktop.app/Contents/Resources/app/cli.js"
     if [ -f "$BIN" ] && [ -f "$CLI" ]; then
-        ELECTRON_RUN_AS_NODE=1 "$BIN" "$CLI" "open" "$@"
-    elif [ -f "$BAT" ]; then
-        "$BAT" "open" "$@"
+        ELECTRON_RUN_AS_NODE=1 "$BIN" "$CLI" "$@"
     else
         echo "GitHub Desktop not found"
     fi
